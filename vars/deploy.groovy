3fa75aebf0d6c754ba8477e66c2a12b0fb8a656f
Priyabrata Mohanty <52962754+Mpriyabrata@users.noreply.github.com>
2021-06-03 10:05:25 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,11 @@
 class deploy {
-  def myData(commitSha) {
-    println("The changed files are : \n")
-    def cmd = 'git log -m -1 "${commitSha}"'
-    def response = cmd.execute().text
-    return response 
-  }
+    static myData(commitSha) {
+        println("The changed files are : \n")
+        def cmd = 'git log -m -1 ' + commitSha
+        def response = cmd.execute().text
+        return response
+    }
+    static void main(String[] args) {
+        println(myData("HEAD"))
+    }
 }
