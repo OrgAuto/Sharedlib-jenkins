3118ae49c01edb6bf61a19beb74a8a8db76711f2
Priyabrata Mohanty <52962754+Mpriyabrata@users.noreply.github.com>
2021-06-03 09:43:41 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,8 @@
+class deploy {
+  def myData(commitSha) {
+    println("The changed files are : \n")
+    def cmd = 'git log -m -1 "${commitSha}"'
+    def response = cmd.execute().text
+    return response 
+  }
+}
