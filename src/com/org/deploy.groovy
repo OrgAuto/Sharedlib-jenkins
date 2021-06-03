52b5d865bab85d53cfbc244c23aa1002de05b55c
Priyabrata Mohanty <52962754+Mpriyabrata@users.noreply.github.com>
2021-06-03 10:40:57 +0530

    Update deploy.groovy

--- src/com/org/deploy.groovy
+++ src/com/org/deploy.groovy
@@ -1,1 +1,12 @@
-
+package com.org
+class deploy {
+    static myData(commitSha) {
+        println("The changed files are : \n")
+        def cmd = 'git log -m -1 ' + commitSha
+        def response = cmd.execute().text
+        return response
+    }
+    static void main(String[] args) {
+        println(myData("HEAD"))
+    }
+}
