f199c2987c3381f4cfe3aaa4ff7db0d740672d08
Priyabrata Mohanty <52962754+Mpriyabrata@users.noreply.github.com>
2021-06-03 12:14:05 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def myData(String commitSha) {
     println("The changed files are : \n")
-    def cmd = ['git log -m -1' + "${commitSha}"]
+    def cmd = ['git log -m -1 ' + "${commitSha}"]
     def response = cmd.execute().text
     return response
 }
