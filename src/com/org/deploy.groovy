dfadee91cde32fea55534a10165ae2717c28cb9f
Priyabrata Mohanty <52962754+Mpriyabrata@users.noreply.github.com>
2021-06-03 11:50:36 +0530

    Update deploy.groovy

--- src/com/org/deploy.groovy
+++ src/com/org/deploy.groovy
@@ -1,14 +1,8 @@
 package com.org
-class deploy implements Serializable {
-    def steps
-    deploy(steps) {this.steps = steps}
-    def myData(commitSha) {
-        println("The changed files are : \n")
-        def cmd = 'git log -m -1 ' + commitSha
-        def response = cmd.execute().text
-        return response
-    }
-//     static void main(String[] args) {
-//         println(myData("HEAD"))
-//     }
+
+def myData(commitSha) {
+    println("The changed files are : \n")
+    def cmd = 'git log -m -1 ' + commitSha
+    def response = cmd.execute().text
+    return response
 }
