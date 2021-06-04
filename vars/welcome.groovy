ba8d5662af14bee6277ce96daa07bdb5aa422087
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-04 22:01:13 +0530

    Shared Library Integration

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,23 +1,21 @@
 #!/usr/bin/env groovy
 def call() {
-    echo "Welcome "
-    script{
-            sh '''
-              git log -m -1 --name-status --pretty=format: HEAD > "${WORKSPACE}"/Logs/commit.log
-            '''
-        }
-    String git_cmd = 'git log -m -1 --name-status --pretty=format: HEAD'
+    println("Welcome ")
     def delta_files = sh(returnStdout: true, script: 'git log -m -1 --name-status --pretty=format: HEAD')
     println("${delta_files}")
     def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel').trim()
     println("${repo_dir}")
-    def dir = "${WORKSPACE}"
-    def commit_file = dir + "/Logs/commit.log"
-    File f = new File(commit_file)
-    ArrayList arr = new ArrayList()
-    for(i in f.readLines()){
-        arr.add(i)
-    }
-    println(arr)
-    f.delete()
+    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
+    def response = sh(returnStdout: true, script: git_name_only_cmd)
+    println("${response}")
+
+//    def dir = "${WORKSPACE}"
+//    def commit_file = dir + "/Logs/commit.log"
+//    File f = new File(commit_file)
+//    ArrayList arr = new ArrayList()
+//    for(i in f.readLines()){
+//        arr.add(i)
+//    }
+//    println(arr)
+//    f.delete()
 }
