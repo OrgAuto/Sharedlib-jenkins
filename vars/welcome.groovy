4cf454d149ca7182140b52771827806c92c202e7
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-04 17:10:33 +0530

    Shared Library Integration

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -6,12 +6,11 @@ def call() {
               git log -m -1 --name-status --pretty=format: HEAD > "${WORKSPACE}"/Logs/commit.log
             '''
         }
-    String repo_dir = sh 'git rev-parse --show-toplevel'
-    println(repo_dir)
+    def repo_dir = sh 'git rev-parse --show-toplevel'
+    println("${repo_dir}")
     def dir = "${WORKSPACE}"
     def commit_file = dir + "/Logs/commit.log"
     File f = new File(commit_file)
-    println(f.path)
     ArrayList arr = new ArrayList()
     for(i in f.readLines()){
         arr.add(i)
