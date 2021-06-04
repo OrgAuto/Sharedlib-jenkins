cac3a72a97094a1772284a3002792ce46e0dcebd
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-04 11:58:14 +0530

    DShared Library test

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -2,7 +2,9 @@ def call() {
     echo "Welcome "
     script{
             sh '''
-                    git log -m -1 --name-status HEAD
+                    git log -m -1 --name-status HEADv > "../Logs/commit.log"
             '''
 	    }
+    File f = new File("../Logs/commit.log")
+    f.readLines()
 }
