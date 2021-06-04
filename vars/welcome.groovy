e8a2ffddfce78c128ee154776b68b519f21a8484
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-04 13:29:56 +0530

    Shared Library Integration

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,19 +1,19 @@
 #!/usr/bin/env groovy
-
 def call() {
     echo "Welcome "
     script{
             sh '''
-                    git log -m -1 --name-status HEAD > "${WORKSPACE}"/Logs/commit.log
-                    ls -l Logs/
-                    pwd
+              git log -m -1 --name-status --pretty=format: HEAD > "${WORKSPACE}"/Logs/commit.log
             '''
-        println("${WORKSPACE}")
-        def dir = "${WORKSPACE}"
-        def commit_file = dir + "/Logs/commit.log"
-        File f = new File(commit_file)
-        println(f.path)
-        println(f.readLines())
-	    }
-
+        }
+    def dir = "${WORKSPACE}"
+    def commit_file = dir + "/Logs/commit.log"
+    File f = new File(commit_file)
+    println(f.path)
+    ArrayList arr = new ArrayList()
+    for(i in f.readLines()){
+        arr.add(i)
+    }
+    println(arr)
+    f.delete()
 }
