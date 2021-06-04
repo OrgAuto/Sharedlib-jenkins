c42d00ab859ed353612d0fe22bfb1247b0687171
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-04 12:59:31 +0530

    Shared Library Integration

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -2,11 +2,11 @@ def call() {
     echo "Welcome "
     script{
             sh '''
-                    git log -m -1 --name-status HEAD > '"${WORKSPACE}"/Logs/commit.log'
+                    git log -m -1 --name-status HEAD > "${WORKSPACE}"/Logs/commit.log
                     ls -l Logs/
             '''
         println("${WORKSPACE}")
-        File f = new File('"${WORKSPACE}"/Logs/commit.log')
+        File f = new File('Logs/commit.log')
         f.readLines()
 	    }
 
