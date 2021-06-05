96b03711df583eb28478a775576fd2c0025cf954
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-05 10:29:13 +0530

    Shared Library Integration

--- vars/org/local/deploy.groovy
+++ vars/org/local/deploy.groovy
@@ -1,16 +1,18 @@
 package org.local
 
-def myData(String commitSha) {
-    println("The changed files are : \n")
-    def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel').trim()
-    println("${repo_dir}")
-    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
-    def response = sh(returnStdout: true, script: git_name_only_cmd)
-    println("${response}")
-}
+class deploy {
+    def myData(String commitSha) {
+        println("The changed files are : \n")
+        def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel').trim()
+        println("${repo_dir}")
+        def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
+        def response = sh(returnStdout: true, script: git_name_only_cmd)
+        println("${response}")
+    }
 
-def myList(String commitSha) {
-    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
-    def response = sh(returnStdout: true, script: git_name_only_cmd)
-    println("${response}")
+    def myList(String commitSha) {
+        def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
+        def response = sh(returnStdout: true, script: git_name_only_cmd)
+        println("${response}")
+    }
 }
