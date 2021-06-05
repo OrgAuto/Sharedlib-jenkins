c4850aa7309b9c3150cac6ee9ad0d6bedfb5308b
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-05 10:51:40 +0530

    Shared Library Integration Class

--- src/com/org/deploy.groovy
+++
@@ -1,8 +0,0 @@
-package com.org
-
-def myData(commitSha) {
-    println("The changed files are : \n")
-    def cmd = 'git log -m -1 ' + commitSha
-    def response = cmd.execute().text
-    return response
-}
