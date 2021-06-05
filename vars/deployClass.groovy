27d67a7e019358b29ad888a11354af55aeb32709
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-05 09:39:33 +0530

    Shared Library Integration

--- vars/deployClass.groovy
+++ vars/deployClass.groovy
@@ -1,11 +1,17 @@
 class deployClass {
     static myData(commitSha) {
         println("The changed files are : \n")
-        def cmd = 'git log -m -1 ' + commitSha
-        def response = cmd.execute().text
-        return response
+        def delta_files = sh(returnStdout: true, script: 'git log -m -1 --name-status --pretty=format: \"${commitSha}\"')
+        println("${delta_files}")
+        def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel').trim()
+        println("${repo_dir}")
+        def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M \"${commitSha}\""
+        def response = sh(returnStdout: true, script: git_name_only_cmd)
+        def arr = []
+        arr.add("${response}")
+        return arr
     }
-//    static void main(String[] args) {
-//        println(myData("HEAD"))
-//    }
+    static void main(String[] args) {
+        println(myData("HEAD"))
+    }
 }
