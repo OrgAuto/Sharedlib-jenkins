08c726637480e7817d2e647d381b6c962d74a2da
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-06 11:24:51 +0530

    Welcome function updated

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,19 +2,19 @@
 
 def myData(String commitSha) {
     println("The changed files are : \n")
-    def delta_files = sh(returnStdout: true, script: 'git log -m -1 --name-status --pretty=format: HEAD')
-    println("${delta_files}")
-    def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel', encoding: 'UTF-8').trim()
-    String my_cmd_response = sh(returnStdout: true, script: "ls -l ${repo_dir}/Logs", encoding: 'UTF-8')
-    println(my_cmd_response)
-    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${commitSha}"
-    def response = sh(returnStdout: true, script: git_name_only_cmd)
-    println("${response}")
-    script {
-
-        sh '''
-            new_dir=`git rev-parse --show-toplevel`
-            ls -l $new_dir
-            '''
-    }
+//    def delta_files = sh(returnStdout: true, script: 'git log -m -1 --name-status --pretty=format: HEAD')
+//    println("${delta_files}")
+//    def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel', encoding: 'UTF-8').trim()
+//    String my_cmd_response = sh(returnStdout: true, script: "ls -l ${repo_dir}/Logs", encoding: 'UTF-8')
+//    println(my_cmd_response)
+//    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${commitSha}"
+//    def response = sh(returnStdout: true, script: git_name_only_cmd)
+//    println("${response}")
+//    script {
+//
+//        sh '''
+//            new_dir=`git rev-parse --show-toplevel`
+//            ls -l $new_dir
+//            '''
+//    }
 }
