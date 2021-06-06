b0a0a59d1265e40fd9f5e10a8144843822550bf7
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-06 09:37:50 +0530

    Welcome function updated

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -6,7 +6,7 @@ def myData(String commitSha) {
     println("${delta_files}")
     def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel').trim()
     println("${repo_dir}")
-    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
+    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${commitSha}"
     def response = sh(returnStdout: true, script: git_name_only_cmd)
     println("${response}")
 }
