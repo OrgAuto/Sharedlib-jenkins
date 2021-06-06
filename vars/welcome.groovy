272c8ca88050d51c490be9c4b6838a768ea8c809
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-06 12:16:04 +0530

    Welcome function updated

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -3,14 +3,17 @@
 
 def call() {
     println("Welcome ")
-    String cmd_modified = "git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD"
-    String cmd_added = "git log -m -1 --name-only --pretty=format: --diff-filter=A HEAD"
-    String cmd_deleted = "git log -m -1 --name-only --pretty=format: --diff-filter=D HEAD"
-    String cmd_top_level = "git rev-parse --show-toplevel"
     String cmd_commit = "git rev-parse HEAD"
+    String cmd_top_level = "git rev-parse --show-toplevel"
+
 
     String repo_dir = sh (returnStdout: true, script: cmd_top_level).trim()
     String current_commit_sha = sh(returnStdout: true, script: cmd_commit).trim()
+
+    String cmd_modified = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${current_commit_sha}"
+    String cmd_added = "git log -m -1 --name-only --pretty=format: --diff-filter=A ${current_commit_sha}"
+    String cmd_deleted = "git log -m -1 --name-only --pretty=format: --diff-filter=D ${current_commit_sha}"
+
     String modified_files = sh(returnStdout: true, script: cmd_modified).trim()
     String new_files = sh(returnStdout: true, script: cmd_added).trim()
     String deleted_files = sh(returnStdout: true, script: cmd_deleted).trim()
@@ -18,6 +21,6 @@ def call() {
     String LogsPath = new File("${repo_dir}", "/Logs").toString();
     println(LogsPath)
     File f = new File(LogsPath.trim() + "commit.log")
-    f.write(modified_files)
+    f.write(modified_files, new_files)
     println(f.readLines())
 }
