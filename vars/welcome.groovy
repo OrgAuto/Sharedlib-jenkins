0d9b982ffca68804fcdc521647c119ae91c45df4
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-07 00:17:46 +0530

    Welcome function updated

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,14 +1,11 @@
 #!/usr/bin/env groovy
-import java.lang.reflect.Array
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 
 def call() {
     println("Welcome ")
     String cmd_commit = "git rev-parse HEAD"
     String cmd_top_level = "git rev-parse --show-toplevel"
-
 
     String repo_dir = sh (returnStdout: true, script: cmd_top_level).trim()
     String current_commit_sha = sh(returnStdout: true, script: cmd_commit).trim()
@@ -21,9 +18,10 @@ def call() {
     def new_files = sh(returnStdout: true, script: cmd_added).trim()
     def deleted_files = sh(returnStdout: true, script: cmd_deleted).trim()
 
-    String LogsPath = new File("${repo_dir}", "/Logs").toString();
-    println(LogsPath)
-    File f = new File(LogsPath.trim() + "commit.log")
+//    String LogsPath = new File("${repo_dir}", "/Logs").toString();
+//    File f = new File(LogsPath.trim() + "commit.log")
+
+    // Print Deleted Files in the current commit
     println("${deleted_files}".split().length + " Deleted Files \n")
 
     // Prepare list of added and modified files
@@ -35,11 +33,13 @@ def call() {
         arr.add(added)
     }
     println("Added: " + arr)
-
+    // Convert String to Path
     for (name in arr) {
         Path path = Paths.get("${name}")
-        println(path)
+        println(path.class)
+
     }
+
 
 
 }
