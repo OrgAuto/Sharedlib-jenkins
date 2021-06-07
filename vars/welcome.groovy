be2d016fd32ae13566ed64679b57d946bfb621bf
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-07 14:21:54 +0530

    Deploy function updated

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -15,24 +15,29 @@ def call() {
                     git rev-parse --show-toplevel
                     set -x
                 '''
-//    String cmd_commit = "git rev-parse HEAD"
-//    String cmd_top_level = "git rev-parse --show-toplevel"
-
-    def repo_dir = sh (returnStdout: true, script: cmd_top_level).trim()
-    String current_commit_sha = sh(returnStdout: true, script: cmd_commit).trim()
     String cmd_modified = '''
+                    set +x
                     git log -m -1 --name-only --pretty=format: --diff-filter=M HEAD
                     set -x
                 '''
     String cmd_added = '''
+                    set +x
                     git log -m -1 --name-only --pretty=format: --diff-filter=A HEAD
                     set -x
                 '''
-
+    String cmd_deleted = '''
+                    set +x
+                    git log -m -1 --name-only --pretty=format: --diff-filter=D HEAD
+                    set -x
+                '''
+//    String cmd_commit = "git rev-parse HEAD"
+//    String cmd_top_level = "git rev-parse --show-toplevel"
 //    String cmd_modified = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${current_commit_sha}"
 //    String cmd_added = "git log -m -1 --name-only --pretty=format: --diff-filter=A ${current_commit_sha}"
-    String cmd_deleted = "git log -m -1 --name-only --pretty=format: --diff-filter=D ${current_commit_sha}"
+//    String cmd_deleted = "git log -m -1 --name-only --pretty=format: --diff-filter=D ${current_commit_sha}"
 
+    def repo_dir = sh (returnStdout: true, script: cmd_top_level).trim()
+    String current_commit_sha = sh(returnStdout: true, script: cmd_commit).trim()
     def modified_files = sh(returnStdout: true, script: cmd_modified).trim()
     def new_files = sh(returnStdout: true, script: cmd_added).trim()
     def deleted_files = sh(returnStdout: true, script: cmd_deleted).trim()
