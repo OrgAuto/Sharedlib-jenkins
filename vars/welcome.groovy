b5b99f7e8a9521881048d6533928ccba353bc920
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-07 10:22:18 +0530

    Welcome function updated

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -50,9 +50,9 @@ def call() {
             String extension = myfile.substring(index + 1)
             println("File Extension is: \n" + extension)
             def myextension = "." + extension
-            def base_name = fileName[0]-"${myextension}"
-            base_files.add(base_name)
-            println("File basename is : " + base_name)
+            base_files.add(fileName[0]-"${myextension}")
+            println("File basename is : " + fileName[0]-"${myextension}")
+            println(fileName[0])
         }
     }
     println("Delta Files: \n" + delta_files)
