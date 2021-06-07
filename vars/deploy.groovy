61297fa795e717385e5fc2b9ff3a0dfa7f0aa1eb
Priyabrata Mohanty <priyabrata.mohanty@autodesk.com>
2021-06-07 12:32:36 +0530

    Deploy function updated

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,20 +1,69 @@
 #!/usr/bin/env groovy
+import java.nio.file.Path
+import java.nio.file.Paths
 
-def myData(String commitSha) {
-    println("The changed files are : \n")
-//    def delta_files = sh(returnStdout: true, script: 'git log -m -1 --name-status --pretty=format: HEAD')
-//    println("${delta_files}")
-//    def repo_dir = sh(returnStdout: true, script: 'git rev-parse --show-toplevel', encoding: 'UTF-8').trim()
-//    String my_cmd_response = sh(returnStdout: true, script: "ls -l ${repo_dir}/Logs", encoding: 'UTF-8')
-//    println(my_cmd_response)
-//    def git_name_only_cmd = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${commitSha}"
-//    def response = sh(returnStdout: true, script: git_name_only_cmd)
-//    println("${response}")
-//    script {
-//
-//        sh '''
-//            new_dir=`git rev-parse --show-toplevel`
-//            ls -l $new_dir
-//            '''
-//    }
+def GetCommit() {
+    String cmd_commit = "git rev-parse HEAD"
+    String current_commit_sha = sh(returnStdout: true, script: cmd_commit).trim()
+    return current_commit_sha
 }
+
+def GetCRepoDir() {
+    String cmd_top_level = "git rev-parse --show-toplevel"
+    String repo_dir = sh(returnStdout: true, script: cmd_top_level).trim()
+    return repo_dir
+}
+
+def GetDeltaFiles(commit_id) {
+    String cmd_modified = "git log -m -1 --name-only --pretty=format: --diff-filter=M ${commit_id}"
+    String cmd_added = "git log -m -1 --name-only --pretty=format: --diff-filter=A ${commit_id}"
+    String cmd_deleted = "git log -m -1 --name-only --pretty=format: --diff-filter=D ${commit_id}"
+
+    def modified_files = sh(returnStdout: true, script: cmd_modified).trim()
+    def new_files = sh(returnStdout: true, script: cmd_added).trim()
+    def deleted_files = sh(returnStdout: true, script: cmd_deleted).trim()
+
+    def delta_files = []
+    for (file in "${modified_files}".split()){
+        delta_files.add(file)
+    }
+    for (added in "${new_files}".split()){
+        delta_files.add(added)
+    }
+    return delta_files
+}
+def GetDeployScripts(DeltaScriptsList) {
+    def deploy_scripts = []
+    for (name in DeltaScriptsList) {
+        Path path = Paths.get("${name}")
+        if (path.toString().contains("Logs")) {     // Get files matching path pattern
+            deploy_scripts.add(path)
+        }
+        else {
+            println("No Matching deployable files")
+        }
+    }
+    return deploy_scripts
+}
+
+def GetExtension(FilePath) {
+    def FileName = FilePath.getFileName()           // GET FILENAME WITHOUT PATH
+    String my_file = FileName.toString()
+    int index = my_file.lastIndexOf('.')
+    if (index > 0) {
+        String extension = my_file.substring(index + 1)
+        println("File Extension is: \n" + extension)
+    def my_extension = "." + extension
+    return my_extension.toString()
+    }
+}
+
+def commit = GetCommit()
+println(commit)
+def repo_dir = GetCRepoDir()
+println(repo_dir)
+def file_list = GetDeltaFiles(commit)
+println(GetDeltaFiles(commit))
+println(GetDeployScripts(file_list))
+
+
